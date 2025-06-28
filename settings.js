318285a4c40120999de9db2d8d5139a5d3b9b782
Frankjs9r <frankmurenzvi8@gmail.com>
2025-06-29 01:33:31 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,13 +6,13 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "malvin~EmFnmAiR#Hk-lNaz7SoMhpaxOBG4OIdkh6j6QIDYrA1l-w3JpGoc",
+SESSION_ID: process.env.SESSION_ID || "malvin~lvEgVAoa#2T6mPK34ZvJepBAneOUfWldx9VTvJCkcSGkUUA9o3hM",
 // add your Session Id make sure it starts with malvin~
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
 BOT_NAME: process.env.BOT_NAME || "ᴍᴀʟᴠɪɴ-xᴅ",
 // add bot name here for menu
-MODE: process.env.MODE || "public",
+MODE: process.env.MODE || "private",
 // make bot public-private-inbox-group 
 
 LINK_WHITELIST: "youtube.com,github.com",
